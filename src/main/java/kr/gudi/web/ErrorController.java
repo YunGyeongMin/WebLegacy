ef24166169858dec06d823b7b1751284895d9e77
GD7 <GD7@DESKTOP-5RQ9K3J>
2020-01-02 13:50:17 +0900

    오류 페이지 연습

---
+++ src/main/java/kr/gudi/web/ErrorController.java
@@ -0,0 +1,25 @@
+package kr.gudi.web;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorController {
+
+	@RequestMapping("/error/{code}")
+	public String error(@PathVariable("code") String code, 
+//						HttpServletResponse res
+						Model model)  {
+		System.out.println(code);
+//		res.getWriter().println("Error Code : " + code);
+		model.addAttribute("code", code);
+		return "error";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/error.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Error</title>
+</head>
+<body>
+	<h1>Error Code : ${code}</h1>
+	<a href="/main">홈으로 이동</a>
+</body>
+</html>
