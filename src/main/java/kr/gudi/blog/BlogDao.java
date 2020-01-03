6e880619bb2b2f0c4aca75acfa2d0df28e459761
GD7 <GD7@DESKTOP-5RQ9K3J>
2020-01-03 10:37:34 +0900

    JDBC 테스트 연습

--- src/main/java/kr/gudi/blog/BlogDao.java
+++ src/main/java/kr/gudi/blog/BlogDao.java
@@ -1,8 +1,18 @@
 package kr.gudi.blog;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BlogDao {
 
+	@Autowired
+	SqlSession sqlSession;
+	
+	public void test() {
+		int result = sqlSession.selectOne("sql.test");
+		System.out.println(result);
+	}
+	
 }
--- src/main/java/kr/gudi/web/controller/TestController.java
+++ src/main/java/kr/gudi/web/controller/TestController.java
@@ -1,8 +1,11 @@
 package kr.gudi.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.gudi.blog.BlogDao;
 
 @Controller
 public class TestController {
@@ -11,6 +14,14 @@ public class TestController {
 	public String test1(@PathVariable("no") String no) {
 		System.out.println(no);
 		return no;
+	}
+	
+	@Autowired
+	BlogDao bd;
+	
+	@RequestMapping("/db")
+	public void db() {
+		bd.test();
 	}
 	
 }
